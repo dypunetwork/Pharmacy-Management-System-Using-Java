4674af766185af5be1bc1ccaa86bb7b4ef50e656
dypunetwork <157093998+dypunetwork@users.noreply.github.com>
2024-08-27 14:27:59 +0530

    Initial commit

--- src/dao/Tables.java
+++ src/dao/Tables.java
@@ -18,7 +18,7 @@ public class Tables {
         try {
             Connection con = ConnectionProvider.getCon();
             Statement st = con.createStatement();
-            st.executeUpdate("CREATE TABLE appuser("
+            st.executeUpdate("CREATE TABLE IF NOT EXISTS appuser("
                     + "appuser_pk INT AUTO_INCREMENT PRIMARY KEY, "
                     + "userRole VARCHAR(200), "
                     + "name VARCHAR(200), "
@@ -30,8 +30,8 @@ public class Tables {
                     + "address VARCHAR(200))"
             );
             st.executeUpdate("insert into appuser (userRole,name,dob,mobileNumber,email,username,password,address) values('Admin','Admin','21-03-2003','7397987460','nagawadetanmay67@gmail.com','admin','admin','India' ) ");
-            st.executeUpdate("create table medicine(medicine_pk int AUTO_INCREMENT primary key, uniqueId varchar(200),name varchar(200),companyName varchar(200),quantity bigint,price bigint)");
-            st.executeUpdate("create table bill(bill_pk int AUTO_INCREMENT primary key,billId varchar(200),billDate varchar(50),totalPaid bigint,generatedBy varchar(50))");
+            st.executeUpdate("create table IF NOT EXISTS medicine(medicine_pk int AUTO_INCREMENT primary key, uniqueId varchar(200),name varchar(200),companyName varchar(200),quantity bigint,price bigint)");
+            st.executeUpdate("create table IF NOT EXISTS bill(bill_pk int AUTO_INCREMENT primary key,billId varchar(200),billDate varchar(50),totalPaid bigint,generatedBy varchar(50))");
             JOptionPane.showMessageDialog(null, "Table Created Successfully");
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e);
